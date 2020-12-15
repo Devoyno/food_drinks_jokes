848e13279e87c139d292e45edd2c0bf5df594e41
BrienB <barrdom@hotmail.com>
2020-12-14 21:24:48 -0500

    added pseudo code for meal recipe search

---
+++ meal_logic.js
@@ -0,0 +1,24 @@
+// When the Random Meal button is clicked, 
+// make a call to the Edamam API to get a random meal recipe
+// put that random meal recipe in a modal???
+
+// When the user checks the radio button for the ingredient 
+// they want to use as the main ingredient,
+// and click the SEARCH button, 
+// it will return a random recipe that uses that main ingredient.
+
+// When the user chooses a meal type from a drop-down menu,
+// and presses the SEARCH button, 
+// it will return a random recipe that is a meal of that type.
+
+// When the user enters the total amount of time that they want 
+// the meal to take to make and press the SEARCH button,
+// it will return a random recipe that will take that long to make.
+
+// When the user likes a recipe and wants to save it,
+// the user can click the save button and it will save that recipe into 
+// local storage (by label???)
+
+// When the user wants to review a saved meal recipe, 
+// When the recall button is clicked,
+// the user can see the saved meal recipes and choose to view one.
