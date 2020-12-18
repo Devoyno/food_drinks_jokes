7b3bfec5c79b0f8ed3370492b4d7ef1389fd97fa
tussingj89 <tussing40@gmail.com>
2020-12-18 17:48:09 -0500

    built drinks api call, jokes api call, and update html

--- drinks.js
+++ drinks.js
@@ -3,9 +3,20 @@ $("#randomDrink").on("click", function() {
     drink(searchType);
 });
 $("#criteria").on("click", function() {
-    var searchType = "filter.php?i=" + $("#ingredient :selected").val();
+    var query = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + $("#ingredient :selected").val();
+    $.ajax ({
+        url: query,
+        method: 'GET'
+}).then(function (response) {
+    console.log(response);
+    var drinkLocation = [Math.floor(Math.random() * response.drinks.length)]
+    console.log(drinkLocation);
+    var drinkID = (response.drinks[drinkLocation].idDrink);
+    var searchType = "lookup.php?i=" + drinkID;
     drink(searchType);
 });
+});
+
 
 
 function drink(searchType) {
@@ -18,7 +29,13 @@ $.ajax ({
     console.log(response);
     console.log(queryURL)
     $("#drink-title").html("<h1>" + response.drinks[0].strDrink);
-    $("#drink-ingredients").text("ingredients: " + response.drinks[0].strIngredient1)
+    $("#drink-image").attr("src", response.drinks[0].strDrinkThumb);
+    // while (response.drinks[0].strIngredient + number != null){
+    //    var ingDiv =  $("<div>").text(response.drinks[0].strMeasure + number + " " + response.drinks[0].strIngredient + number);
+    //     $("#drink-ingredients").append(ingDiv);
+    //     var number = number + 1;
+    // };
+    $("#drink-ingredients").text(response.drinks[0].strMeasure1 + "  " + response.drinks[0].strIngredient1)
     $("#drink-directions").text("Directions: " + response.drinks[0].strInstructions);
 
 });
