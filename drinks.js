5073a3103a077dc2431da6166720770254e2bde0
tussingj89 <tussing40@gmail.com>
2020-12-20 15:21:06 -0500

    updated the drinks api and fixed the index.html button issue

--- drinks.js
+++ drinks.js
@@ -1,3 +1,24 @@
+var DrinkSave = [];
+
+// saved drinks 
+function getSaveddrinks(){
+    if(!localStorage.getItem("DrinkSave")){
+        return;
+    }
+    savedDrinks = [JSON.parse(localStorage.getItem("DrinkSave"))];
+    for (i = 0; i < savedDrinks[0].length; i++){
+        $('<option/>').text(savedDrinks[0][i].name).appendTo("#StoredDrinks");
+    };
+}
+
+getSaveddrinks();
+
+$("#StoredDrinks").change(function(){
+    var Drink_name = $("#StoredDrinks").val();
+    var searchType = "search.php?s=" + Drink_name;
+    $("#drink-ingredients").empty();
+    drink(searchType);
+})
 
 // random button 
 $("#randomDrink").on("click", function(event) {
@@ -5,6 +26,7 @@ $("#randomDrink").on("click", function(event) {
     var searchType = "random.php";
     $("#drink-ingredients").empty();
     drink(searchType);
+    console.log(DrinkSave);
 });
 // by ingredient button 
 $("#criteria").on("click", function(event) {
@@ -35,7 +57,7 @@ $.ajax ({
 }).then(function (response) {
     console.log(response);
     console.log(queryURL)
-    $("#drink-title").html("<h1>" + response.drinks[0].strDrink);
+    $("#drink-title").html("<h1><strong>" + response.drinks[0].strDrink);
     $("#drink-image").attr("src", response.drinks[0].strDrinkThumb);
 
     while (response.drinks[0]["strIngredient" + number] != null){
@@ -47,13 +69,15 @@ $.ajax ({
     $("#drink-directions").text("Directions: " + response.drinks[0].strInstructions);
 
 
-    // save button 
-    var DrinkSave = [];
-    $("#drinkSave").on("click", function(){
-        localStorage.getItem("DrinkSave");
-        var textContent = {name: response.drinks[0].strDrink, drinkId: response.drinks[0].idDrink};
-        DrinkSave.push(textContent);
-        localStorage.setItem("DrinkSave", JSON.stringify(DrinkSave));
-    });
+   
 });
 }
+
+ // save button 
+ $("#drinkSave").on("click", function(){
+    localStorage.getItem("DrinkSave");
+    var textContent = {name: $("#drink-title").text()};
+    console.log(DrinkSave);
+    DrinkSave.push(textContent);
+    localStorage.setItem("DrinkSave", JSON.stringify(DrinkSave));
+});
